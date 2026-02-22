8d83d75ba428228044589b4998525711f9fea600
Brayan-Camargo <brayancamargoramirez@gmail.com>
2026-02-22 00:46:39 -0600

    Se configuro la generacion del Token JWT, se implemento el PasswordEncoder con BCrypt y se valido la autenticacion exitosa mediante un test de integracion y pruebas en Insomnia

---
+++ src/main/java/com/aluracursos/forohub/infra/security/TokenService.java
@@ -0,0 +1,38 @@
+package com.aluracursos.forohub.infra.security;
+
+import com.aluracursos.forohub.domain.usuario.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String apiSecret;
+
+    public String generarToken(Usuario usuario) {
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256(apiSecret);
+            return JWT.create()
+                    .withIssuer("ForoHub")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(generarFechaExpiracion())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error al generar el token JWT", exception);
+        }
+    }
+
+    private Instant generarFechaExpiracion() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-06:00"));
+    }
+
+}
--- src/test/java/com/aluracursos/forohub/ForohubApplicationTests.java
+++ src/test/java/com/aluracursos/forohub/ForohubApplicationTests.java
@@ -1,13 +1,27 @@
 package com.aluracursos.forohub;
 
+import com.aluracursos.forohub.domain.usuario.Usuario;
+import com.aluracursos.forohub.domain.usuario.UsuarioRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class ForohubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UsuarioRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void crearUsuario() {
+        // Usamos el constructor directamente en lugar de los setters
+        var usuario = new Usuario(null, "brayan.camargo", passwordEncoder.encode("123456"));
+
+        repository.save(usuario);
+        System.out.println("Usuario creado con éxito!");
+    }
 }
