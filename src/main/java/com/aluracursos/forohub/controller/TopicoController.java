dfd472be5d693896980dbd3961cd75fd9c666c60
Brayan-Camargo <brayancamargoramirez@gmail.com>
2026-02-21 18:54:33 -0600

    Se agrego DetallesTopico para poder ver en insomnia los errores como debe y en vez de mandar un 200 nos mande un 201, adicionalmente se modifico TopicoConotroller para  agregar Transaccional y para modificar el guardado de la informacion

--- src/main/java/com/aluracursos/forohub/controller/TopicoController.java
+++ src/main/java/com/aluracursos/forohub/controller/TopicoController.java
@@ -1,14 +1,18 @@
 package com.aluracursos.forohub.controller;
 
+import com.aluracursos.forohub.domain.topico.DatosDetalleTopico;
 import com.aluracursos.forohub.domain.topico.DatosRegistroTopico;
 import com.aluracursos.forohub.domain.topico.Topico;
 import com.aluracursos.forohub.domain.topico.TopicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/topicos")
@@ -18,7 +22,10 @@ public class TopicoController {
     private TopicoRepository repository;
 
     @PostMapping
-    public void registrarTopico(@RequestBody @Valid DatosRegistroTopico datos) {
-        repository.save(new Topico(datos));
+    @Transactional
+    public ResponseEntity registrarTopico(@RequestBody @Valid DatosRegistroTopico datos, UriComponentsBuilder uriBuilder) {
+        Topico topico = repository.save(new Topico(datos));
+        var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DatosDetalleTopico(topico));
     }
 }
